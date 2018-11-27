9f2fe036db275a2c95b803f4c36c2dc3fde8b007
Zein Ersyad <zeinersyad@gmail.com>
2018-11-27 19:16:19 +0700

    update error tidak teregister dalam kamus

--- klasifikasi.py
+++ klasifikasi.py
@@ -1,5 +1,3 @@
-import sys
-
 kamus_S = ['aku', 'saya', 'kamu', 'dia', 'anda']
 kamus_P = ['sedang makan', 'lagi tidur']
 kamus_O = ['nasi goreng', 'bakso', 'tempe']
@@ -9,11 +7,13 @@ arrayKalimat = []
 kevalidan_kalimat = [False, False, False, False]
 
 counter_sama = 0
+counter_redo = 0
 
 def rekursifKata(kamus, k, l):
     global counter_sama
+    global counter_redo
     global arrayKalimat
-    
+
     try:
         if counter_sama is len(kamus[k]):
             try:
@@ -26,14 +26,23 @@ def rekursifKata(kamus, k, l):
     except IndexError:
         return False
 
-    print(f'{kamus[k][l], arrayKalimat}, {kamus[k]}')
+    # print(f'{kamus[k][l], arrayKalimat}, {kamus[k]}')
     if kamus[k][l] is arrayKalimat[0] :
         counter_sama += 1
+        counter_redo = 0
         arrayKalimat.pop(0)
         if not arrayKalimat : return True
         return rekursifKata(kamus, k, l+1)
     else:
         if counter_sama is 0:
+            # print(counter_redo)
+            if len(kamus) is counter_redo:
+                for i in range(0, len(arrayKalimat)):
+                    arrayKalimat.pop(0)
+                    if arrayKalimat[0] == ' ': 
+                        arrayKalimat.pop(0) 
+                        break
+            counter_redo+=1
             return rekursifKata(kamus, k+1, l)
         else : 
             try:
@@ -44,7 +53,9 @@ def rekursifKata(kamus, k, l):
     
 
 def main():
-    kalimat = 'saya sedang makan di masjid'
+    # kalimat = 'saya lagi tidur tempe di masjid'
+    kalimat = input('Masukan kalimat : ')
+    kalimat = kalimat.lower()
 
     for x in kalimat:
         arrayKalimat.append(x)
@@ -63,7 +74,7 @@ def main():
     if kevalidan_kalimat[3] :
         print(f'K valid')
 
-    print(arrayKalimat)
+    # print(arrayKalimat)
 
 if __name__ == "__main__":
     main()
